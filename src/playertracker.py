03fb177b8e106e80413ef6a9d4c7ce9dae0fe840
Diesel <dieselfi@pm.me>
2025-06-04 20:57:32 +0300

    Fix capitalization of Radiant Oathplate

--- src/playertracker.py
+++ src/playertracker.py
@@ -29,7 +29,7 @@ OTHER_POINT_CALCULATOR = {
     "Elite CA": 2,
     "Master CA": 3,
     "Grandmaster CA": 6,
-    "Radiant oathplate": 3
+    "Radiant Oathplate": 3
 }
 RANKS_EHP_EHB = {
     1 : 100,
@@ -328,7 +328,7 @@ def get_base_player_tracker(gamemode : str):
                 "Elite CA": False,
                 "Master CA": False,
                 "Grandmaster CA": False,
-                "Radiant oathplate": False
+                "Radiant Oathplate": False
             },
             "Total XP": 0,
             "Points": 0,
@@ -418,7 +418,7 @@ def track_all_players(verbose=False):
             player_tracker[member]["Other"]["Elite CA"] = True if member_data[6] == "TRUE" else False
             player_tracker[member]["Other"]["Master CA"] = True if member_data[7] == "TRUE" else False
             player_tracker[member]["Other"]["Grandmaster CA"] = True if member_data[8] == "TRUE" else False
-            player_tracker[member]["Other"]["Radiant oathplate"] = True if member_data[9] == "TRUE" else False
+            player_tracker[member]["Other"]["Radiant Oathplate"] = True if member_data[9] == "TRUE" else False
             player_tracker[member]["Points"] = compute_points(player_tracker[member], verbose)
             player_tracker[member]["Rank"] = compute_rank(player_tracker[member])
     return player_tracker
@@ -484,7 +484,7 @@ def track_player(member: str, verbose=False):
             player_tracker["Other"]["Elite CA"] = True if member_data[6] == "TRUE" else False
             player_tracker["Other"]["Master CA"] = True if member_data[7] == "TRUE" else False
             player_tracker["Other"]["Grandmaster CA"] = True if member_data[8] == "TRUE" else False
-            player_tracker["Other"]["Radiant oathplate"] = True if member_data[9] == "TRUE" else False
+            player_tracker["Other"]["Radiant Oathplate"] = True if member_data[9] == "TRUE" else False
             player_tracker["Points"] = compute_points(player_tracker, verbose)
             player_tracker["Rank"] = compute_rank(player_tracker)
             break
